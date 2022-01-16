5e127487d14c814055f10a9486cd2af9a115cb45
yyh <913217005@qq.com>
2022-01-16 21:50:33 +0800

    init

--- JPS/JPS.h
+++ JPS/JPS.h
@@ -4,5 +4,39 @@
 
 #ifndef JPS_JPS_H
 #define JPS_JPS_H
+#include<iostream>
+#include<mutex>
+#include<vector>
+#include<chrono>
+const size_t MAPSIZE = 1000;
+class JPS{
+ public:
+  JPS();
+  virtual ~JPS();
+  JPS(const JPS&) = delete;
+  JPS(JPS&&) = delete;
+  JPS& operator=(const JPS&) = delete;
 
+  struct point{
+    std::pair<int, int>loc;
+    int G;
+    int H;
+    int F;
+  };
+
+  std::mutex searchMutex;
+  static std::vector<point>map[MAPSIZE];
+  static std::vector<point>openList[MAPSIZE];
+  static std::vector<point>closeList[MAPSIZE];
+  static std::vector<point>path[MAPSIZE];
+
+  void initMap(const int x, const int y)const;
+
+  void searchJumpPoint(const int x, const int y, const point f)const;
+
+  void updateValue(point f)const;
+
+  void genaratePath()const;
+
+};
 #endif //JPS_JPS_H
--- JPS/cmake-build-debug/CMakeFiles/JPS.dir/flags.make
+++ JPS/cmake-build-debug/CMakeFiles/JPS.dir/flags.make
@@ -4,7 +4,7 @@
 # compile CXX with /usr/bin/c++
 CXX_DEFINES = 
 
-CXX_INCLUDES = 
+CXX_INCLUDES = -I/home/yyh/JPS_/JPS/JPS/include -I/home/yyh/JPS_/JPS/JPS/src
 
 CXX_FLAGS = -g -std=gnu++14
 
