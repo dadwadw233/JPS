362f9c38f4aac245cc3c70ede79b6ece6611cdeb
yyh <913217005@qq.com>
2022-01-16 20:11:15 +0800

    first commit

--- JPS/cmake-build-debug/cmake_install.cmake
+++ JPS/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/yyh/JPS_/JPS
+# Install script for directory: /home/yyh/JPS_/JPS/JPS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/yyh/JPS_/JPS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/yyh/JPS_/JPS/JPS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
